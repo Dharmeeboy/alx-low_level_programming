4d6ebd5d8b03e707b5890441dcfec2f2265da8db
Damilola Oyewole <odamilola91@yahoo.com>
2022-09-09 04:08:00 +0000

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,12 @@
 #include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: program prints a text with a new line at the end
+ *
+ * Return: Always 0 (Succes)
+ */
+
 int main (void)
 {
 	printf("with proper grammar, but the outcome is a piece of art\n");
