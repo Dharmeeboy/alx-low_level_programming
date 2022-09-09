bc79a53ed28633dadada78a4cd35178c24747618
Damilola Oyewole <odamilola91@yahoo.com>
2022-09-09 04:55:17 +0000

    6-szie

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -14,8 +14,8 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("Size of a char: %ld byte(s)\n", (long unsigned int) sizeof(a));
-	printf("Size of an int: %ld byte(s)\n", (long unsigned int) sizeof(b));
+	printf("Size of a char: %ld byte(s)\n", (unsigned long) sizeof(a));
+	printf("Size of an int: %ld byte(s)\n", (unsigned long) sizeof(b));
 	printf("Size of a long int: %ld byte(s)\n", sizeof(c));
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(d));
 	printf("Size of a float: %ld byte(s)\n", sizeof(e));
