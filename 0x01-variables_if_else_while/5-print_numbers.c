14b2f5631bd6af365effde3347212860910e3519
Damilola Oyewole <odamilola91@yahoo.com>
2022-09-10 04:04:12 +0000

    print numbers less than 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main-print numbers below 10
+ *
+ * result: Always(success)
+ *
+ */
+
+int main(void)
+{
+	for(int c = 0; c < 10; c++)
+	{
+		printf("%d", c);
+	}
+	printf("\n");
+	return (0);
+}
+
