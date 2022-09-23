aa9cd35b4b13587b8d7f5dec5732e94ca108c651
Damilola Oyewole <odamilola91@yahoo.com>
2022-09-23 01:11:03 +0000

    corrections and fixes

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -16,8 +16,8 @@ void print_line(int n)
 	else
 	{
 		int i;
-		
-		for (i = 1; i <= n; i++)
+
+		for (i = 0; i <= n; i++)
 		{
 			_putchar('_');
 		}
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,8 +14,8 @@ void print_diagonal(int n)
 	else
 	{
 		int i, j;
-		
-		for (i = 0; i < n; i++)
+
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j < n; j++)
 			{
