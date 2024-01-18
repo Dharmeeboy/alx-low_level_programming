63001e6e25a08180d564cc3bc8f2bcccc6888651
Dharmeeboy <odamilola91@yahoo.com>
2024-01-18 22:07:05 +0000

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
+/*
+* main - entry point
+* A programs thats uses puts
+*
+*/
+
 #include <stdio.h>
-int main (void)
+int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
-	return(0);
+    puts("""Programming is like building a multilingual puzzle");
+    return(0);
 }
