3c2813afd3dce6d43ab51006ef6d36512154c66b
Dharmeeboy <odamilola91@yahoo.com>
2024-01-19 10:05:13 +0000

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+/**
+* main - A program that prints the size of various types on the computer.
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+printf("Size of an int: %lu byte(s)\n", sizeof(int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+print("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
+print("Size of a float: %lu byte(s)\n", sizeof(float));
+}
