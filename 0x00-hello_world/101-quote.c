6b1385ace626a1d982b477fde790e646b6664a12
Dharmeeboy <odamilola91@yahoo.com>
2024-01-19 10:39:46 +0000

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+/**
+* main - A program that prints standard error
+*
+* Description: 'the program's description'
+*
+* Return: Always 0 (Success)
+*/
+
+#include <stdio.h>
+int main(void)
+{
+fprintf(stderr, "%s", "nd that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+return (0);
+}
