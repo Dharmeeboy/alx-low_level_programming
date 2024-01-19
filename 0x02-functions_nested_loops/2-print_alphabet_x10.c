bbe44b0c24751da0677d8e6f36e9675fb7eea394
Dharmeeboy <odamilola91@yahoo.com>
2024-01-19 23:08:52 +0000

    print alphabets x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_alphabet_x10 - Entry Point
+ * Return: 0 (success)
+ */
+
+void print_alphabet_x10(void);
+{
+char c;
+char d;
+for (d = 0; d < 10; d++)
+{
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+}
+_putchar('\n');
+}
