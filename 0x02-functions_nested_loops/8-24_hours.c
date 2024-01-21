7d020ce73a4908c150173a4aa394282f57f2f10e
Dharmeeboy <odamilola91@yahoo.com>
2024-01-21 21:28:51 +0000

    24hrs

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -12,13 +12,13 @@ void jack_bauer(void)
 {
 int a, b, c, d;
 
-for (a = 0; a < 3; a++)
+for (a = 0; a <= 2; a++)
 {
-for (b = 0; b < 10; b++)
+for (b = 0; b <= 9; b++)
 {
-for (c = 0; c < 6; c++)
+for (c = 0; c <= 5; c++)
 {
-for (d = 0; d < 10; d++)
+for (d = 0; d <= 9; d++)
 {
 if ((a + b) <= 5)
 {
