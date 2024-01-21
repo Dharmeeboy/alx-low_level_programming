a892b36476838573ba2d8a6a99122e7f54f90364
Dharmeeboy <odamilola91@yahoo.com>
2024-01-21 22:15:59 +0000

    9-timestable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -16,8 +16,15 @@ for (a = 0; a <= 9; a++)
 for (b = 0; b <= 9; b++)
 {
 c = a * b;
+if (c > 9)
+{
 putchar((c / 10) + '0');
 putchar((c % 10) + '0');
+}
+else
+{
+putchar((c % 10) + '0');
+}
 if (b <= 8)
 {
 putchar(',');
