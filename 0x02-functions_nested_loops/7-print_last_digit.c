db21cc23b38782c06478c29b021a709dbf82de6b
Dharmeeboy <odamilola91@yahoo.com>
2024-01-21 00:38:41 +0000

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+* print_last_digit - Entry Point
+*
+* @c: parameter to be checked
+* Return: 0 (success)
+*/
+
+int print_last_digit(int c)
+{
+int last_digit;
+last_digit = c % 10;
+if (last_digit < 0)
+{
+return (last_digit * -1);
+}
+else
+{
+return (last_digit);
+}
+}
