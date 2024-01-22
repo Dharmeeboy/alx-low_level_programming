0c7483d03c3eed05bfd70261c7df6647b754b1ae
Dharmeeboy <odamilola91@yahoo.com>
2024-01-22 20:55:20 +0000

    diagnonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,7 @@ if (n > 0)
 {
 for (i = 0; i < n; i++)
 {
-for (j = 0; j < n; j++)
+for(j = 0; j < n; j++)
 {
 if (j != i)
 {
@@ -25,7 +25,7 @@ else
 _putchar(92);
 }
 }
-_putchar('\n);
+_putchar('\n');
 }
 }
 else
@@ -33,4 +33,3 @@ else
 _putchar('\n');
 }
 }
-
