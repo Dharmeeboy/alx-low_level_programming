9a390d463a8df569bc28aeb144e4316812062b09
Dharmeeboy <odamilola91@yahoo.com>
2024-01-22 14:24:02 +0000

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* mul - multiplies two digits
+* @a: first digit
+* @b: second digit
+* Return: 0 (success)
+*/
+
+int mul(int a, int b)
+{
+int product;
+product = a * b;
+return (product);
+}
