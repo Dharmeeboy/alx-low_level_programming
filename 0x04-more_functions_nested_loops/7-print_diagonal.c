1b737e80c36bf2240d528e71eec556ab425635d7
Dharmeeboy <odamilola91@yahoo.com>
2024-01-22 20:43:26 +0000

    diagnonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,20 +10,27 @@
 void print_diagonal(int n)
 {
 int i, j;
+if (n > 0)
+{
 for (i = 0; i < n; i++)
 {
 for (j = 0; j < n; j++)
 {
 if (j != i)
 {
-_putchar(32);
+putchar(32);
 }
 else
 {
-_putchar(92);
+putchar(92);
 }
 }
 printf("\n");
+}
+}
+else
+{
+_putchar('\n');
 }
 }
 
