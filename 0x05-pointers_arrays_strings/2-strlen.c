9171a2872f8e44c65006c2e7a833421a019c5233
Dharmeeboy <odamilola91@yahoo.com>
2024-01-24 07:33:00 +0000

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* _strlen - returns the length of a string
+* @s:  string to be counted
+* Return: always 0
+*/
+
+int _strlen(char *s)
+{
+int count;
+int i;
+
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] <= 128)
+{
+count++;
+}
+}
+return (count);
+}
