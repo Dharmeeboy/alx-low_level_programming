bedaf4fbcab2372fec27573bdf24d93645cc2ae6
Dharmeeboy <odamilola91@yahoo.com>
2024-01-24 18:54:41 +0000

    puts hald

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* puts_half - print every other string
+* @str: string to be printed
+* Return: always 0
+*/
+
+
+void puts_half(char *str)
+{
+int i = 0, a, h;
+
+while (str[i] != '\0')
+{
+i++;
+}
+if (i % 2 == 0)
+{
+h = i / 2;
+}
+else
+{
+h = (i - 1) / 2;
+}
+
+for (a = 0; str[a] != '\0'; a++)
+{
+if (a >= 5)
+{
+putchar(str[a]);
+}
+}
+putchar('\n');
+}
+
