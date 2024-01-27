b75cafacc2d9630d351fc4963763fb2b00cd6bbf
Dharmeeboy <odamilola91@yahoo.com>
2024-01-27 21:03:37 +0000

    strcmp

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,32 +12,32 @@
 
 int _strcmp(char *s1, char *s2)
 {
-    int i = 0, j = 0;
-    while (s1[i] != '\0')
-    {
-        while(s2[j] != '\0')
-        {
-            if (s1[i] != s2[j])
-            {
-                if (s1[i] == ' ')
-                {
-                    return (0 - s2[j]);
-                }
-                else if (s2[j] == ' ')
-                {
-                    return (s1[i] - 0);
-                }
-                else
-                {
-                    return (s1[i] - s2[j]);
-                }
-            }
-            else
-            {
-                i++;
-                j++;
-            }
-        }
-    }
+int i = 0, j = 0;
+while (s1[i] != '\0')
+{
+while (s2[j] != '\0')
+{
+if (s1[i] != s2[j])
+{
+if (s1[i] == ' ')
+{
+return (0 - s2[j]);
+}
+else if (s2[j] == ' ')
+{
+return (s1[i] - 0);
+}
+else
+{
+return (s1[i] - s2[j]);
+}
+}
+else
+{
+i++;
+j++;
+}
+}
+}
 return (0);
 }
