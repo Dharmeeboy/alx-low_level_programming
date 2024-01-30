92786735de0c48745efc954af12a8e686c211aa7
Dharmeeboy <odamilola91@yahoo.com>
2024-01-30 11:19:45 +0000

    mozarts code

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* leet - encode text
+* @p: string to be converted
+* Return: returns pointer to the encoded text
+*/
+
+
+char *leet(char *p)
+{
+    char letters[] = {'a', 'e', 'o', 't', 'l', 'A', 'E', 'O', 'T', 'L'};
+    char numbers[] = {'4', '3', '0', '7', '1', '4', '3', '0', '7', '1'};
+    int i, d, m = sizeof(letters)/ sizeof(char);
+    for (i = 0; p[i] != '\0'; i++)
+    {
+        for (d = 0; d < m; d++)
+        {
+            if (p[i] == letters[d])
+            {
+                p[i] = numbers[d];
+            }
+        }
+    }
+    return (p);
+}
