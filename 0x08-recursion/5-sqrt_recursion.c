beb1a084e4f994b1090936d1115a646d07b74b6a
Dharmeeboy <odamilola91@yahoo.com>
2024-02-26 20:42:37 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -9,33 +9,33 @@
 
 int _checker(int a, int b)
 {
-    if(b == 0 || b == 1)
-    {
-        return (1);
-    }
-    else if ((a * a) == b)
-    {
-        return (a);
-    }
-    else if((a * a) < b)
-    {
-        return _checker(a + 1, b);
-    }
-    return (-1);
+if(b == 0 || b == 1)
+{
+return (1);
+}
+else if ((a * a) == b)
+{
+return (a);
+}
+else if((a * a) < b)
+{
+return _checker(a + 1, b);
+}
+return (-1);
 }
 
 
 
 int _sqrt_recursion(int n)
 {
-    int x;
-    x = 0;
-    if (x < 0)
-    {
-        return (-1);
-    }
-    else
-    {
-        return (_checker(x, n));
-    }
+int x;
+x = 0;
+if (x < 0)
+{
+return (-1);
+}
+else
+{
+return (_checker(x, n));
+}
 }
