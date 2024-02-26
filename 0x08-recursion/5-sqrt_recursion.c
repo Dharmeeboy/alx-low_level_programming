806996497b844b078d732ba0b128ed916020ab04
Dharmeeboy <odamilola91@yahoo.com>
2024-02-26 20:54:41 +0000

    square root

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,14 +2,15 @@
 #include "main.h"
 
 /**
-* _sqrt_recursion - calculates the square
-* @n: number
-* Return: returns square root of n
-*/
+ * _checker - Checks the function _sqrt_recursion
+ * @a: Integer
+ * @b: Integer
+ * Return: A checker for the above function
+ */
 
 int _checker(int a, int b)
 {
-if(b == 0 || b == 1)
+if (b == 0 || b == 1)
 {
 return (1);
 }
@@ -17,14 +18,19 @@ else if ((a * a) == b)
 {
 return (a);
 }
-else if((a * a) < b)
+else if ((a * a) < b)
 {
-return _checker(a + 1, b);
+return (_checker(a + 1, b));
 }
 return (-1);
 }
 
 
+/**
+* _sqrt_recursion - calculates the square
+* @n: number
+* Return: returns square root of n
+*/
 
 int _sqrt_recursion(int n)
 {
