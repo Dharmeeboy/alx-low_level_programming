cfbf38dba8c4ea9cb523a4f137afde733f70b4a5
Dharmeeboy <odamilola91@yahoo.com>
2024-02-28 17:44:13 +0000

    print number of args passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* main - Entry
+* @argc: name of the program
+* @argv: argumnets for the program
+* Return: Always Success (0)
+*/
+
+
+int main(int argc, char *argv[])
+{
+(void)argv;
+printf("%d\n", argc - 1);
+return (0);
+}
