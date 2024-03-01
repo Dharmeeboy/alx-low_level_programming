d32de525972a0af2f18e6f2cc1b9bd98cc4b72a6
Dharmeeboy <odamilola91@yahoo.com>
2024-03-01 09:16:09 +0000

    betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,19 +7,23 @@
  * Return: pointer to the duplicated string
  */
 
-char *_strdup(char *str) {
-  char *new_str;
-  unsigned int i, len;
-  if (str == NULL) {
-    return (NULL);
-  }
-  new_str = malloc(sizeof(char) * (strlen(str) + 1));
-  if (new_str == NULL) {
-    return (NULL);
-  }
-  len = strlen(str);
-  for (i = 0; i < len; i++) {
-    new_str[i] = str[i];
-  }
-  return (new_str);
+char *_strdup(char *str)
+{
+char *new_str;
+unsigned int i, len;
+if (str == NULL)
+{
+return (NULL);
+}
+new_str = malloc(sizeof(char) * (strlen(str) + 1));
+if (new_str == NULL)
+{
+return (NULL);
+}
+len = strlen(str);
+for (i = 0; i < len; i++)
+{
+new_str[i] = str[i];
+}
+return (new_str);
 }
