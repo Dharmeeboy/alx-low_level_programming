dd1f17f095e5cd53ff4db63c21a312d0405125da
Dharmeeboy <odamilola91@yahoo.com>
2024-03-01 10:48:02 +0000

    handle nulls

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,9 +12,18 @@ char *str_concat(char *s1, char *s2)
 {
 char *concat;
 unsigned int i, j;
-if (s1 == NULL || s2 == NULL)
+if (s1 == NULL && s2 != NULL)
 {
-return (NULL);
+s1 = "";
+}
+else if (s1 != NULL && s2 == NULL)
+{
+s2 = "";
+}
+else if (s1 == NULL && s2 == NULL)
+{
+s1 = "";
+s2 = "";
 }
 concat = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
 if (concat == NULL)
