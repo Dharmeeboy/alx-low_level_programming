1a53ab31757ebbacebfa3e696a03ef65695ac01b
Dharmeeboy <odamilola91@yahoo.com>
2024-03-11 22:50:49 +0000

    print name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+  * main - prints the file from which it is run
+  * Return: Always 0.
+  */
+
+int main(void)
+{
+printf(__FILE__ "\n");
+return (0);
+}
